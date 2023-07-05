f923c0192da9387368f0def80115ee27dcbeb086
dristosun <107073369+dristosun@users.noreply.github.com>
2023-07-05 14:29:32 +0300

    Add minimal configurations + background photo

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
         static: {
             directory: path.resolve(__dirname, 'dist')
         },
+        port:3000,
         open: true,
         compress: true,
         historyApiFallback: true,

15293fecf257539b73fe0c90aed371e8853d4b18
dristosun <107073369+dristosun@users.noreply.github.com>
2023-07-06 00:48:57 +0300

    Fill in the first section of info in Javascript

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import Styles from './styles/styles.scss';
+import moment from 'moment';
+
 import boilerplateCode from './boilerplate';
 let currentMeasurements = 'C';
 
-async function getLocationInfo(location="London") {
-    const info = await fetch("http://api.weatherapi.com/v1/forecast.json?key=53a560bacc8a4f86884150535230407&q=London&days=7&aqi=no&alerts=no", {mode: 'cors'});
+async function getLocationInfo(location) {
+    const info = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=53a560bacc8a4f86884150535230407&q=${location}&days=7&aqi=no&alerts=no`, {mode: 'cors'});
     const london = info.json();
 
     return london;
 }
 
-getLocationInfo()
-.then(console.log)
+function fillFormWithInfo(location) {
+    getLocationInfo(location)
+    .then(result => {
+        console.log(result);
+        document.querySelector('.name').textContent = result.location.name;
+        const date = new Date(result.current.last_updated)
+        const dayMonthYr = date.toDateString();
+        const time = `${date.getHours < 10 ? `0${date.getHours}` : date.getHours()}:${date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()}`;
+        document.querySelector('.dayInfo').textContent = dayMonthYr;
+        document.querySelector('.currentTime').textContent = time;
+        document.querySelector('.weatherDescription').textContent = result.current.condition.text;
+        if (currentMeasurements === "C") {
+            document.querySelector('.degrees').textContent = `${result.current.temp_c} °C`
+        }
+        else {
+            document.querySelector('.degrees').textContent = `${result.current.temp_f} °F`;
+        }
+    });
+}
 
 window.onload = function() {
     boilerplateCode();
+    fillFormWithInfo("Bucharest");
 }

5741d4e56008ba489341bd8a0da2e8fa2c1c5637
dristosun <107073369+dristosun@users.noreply.github.com>
2023-07-05 16:40:11 +0300

    Write HTML structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import Styles from './styles/styles.scss'
+
+async function getLocationInfo(location="London") {
+    const info = await fetch("http://api.weatherapi.com/v1/forecast.json?key=53a560bacc8a4f86884150535230407&q=London&days=7&aqi=no&alerts=no", {mode: 'cors'});
+    const london = info.json();
+
+    return london;
+}
+
+getLocationInfo()
+.then(console.log)
